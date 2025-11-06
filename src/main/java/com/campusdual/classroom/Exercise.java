7d80f5d8578a890af33874a0d20993ec20b2f5f3
Lucas Vázquez <lucasvazquezarias@gmail.com>
2025-11-06 16:20:12 +0100

    Exercise 20

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -1,14 +1,27 @@
 package com.campusdual.classroom;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Exercise {
 
     public static List<Person> getPeopleList() {
+        List<Person> list = new ArrayList<>();
+        list.add(new Person("John", "Smith"));
+        list.add(new Teacher("María", "Montessori", "Educación" ));
+        list.add(new PoliceOfficer("Jake", "Peralta", "B-99"));
+        list.add(new Doctor("Gregory", "House", "Nefrología e Infectonología"));
+
+        return list;
+
+
 
     }
 
     public static void showPeopleDetails(List<Person> stringList) {
+        for (Person person : stringList) {
+            person.getDetails();
+        }
 
     }
 
